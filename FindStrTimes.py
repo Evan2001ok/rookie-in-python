840480cffd490fe0b534e9c3addf223b8798a690
YIFAN ZHU <154294424+Evan2001ok@users.noreply.github.com>
2024-06-10 08:31:45 +0800

    Add files via upload

---
+++ FindStrTimes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+def cout_ch(words, word):
+    times = 0
+    for time in words:
+        if time == word:
+            times += 1 
+    return times
+cout_ch("the goofy doom of the balloon goons","o")           
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
