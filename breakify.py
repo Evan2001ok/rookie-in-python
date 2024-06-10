2890ddf62e395f0c6eb8fac65477b01ca0e1aa74
YIFAN ZHU <154294424+Evan2001ok@users.noreply.github.com>
2024-06-11 04:02:47 +0800

    Add files via upload

---
+++ breakify.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def breakify():
+    lines = ["Haiku frogs in snow",
+         "A limerick came from Nantucket",
+         "Tetrametric drum-beats thrumming, Hiawathianic rhythm."]
+    return '<br>'.join(lines)
+breakify()
+
+
+# In[ ]:
+
+
+
+
