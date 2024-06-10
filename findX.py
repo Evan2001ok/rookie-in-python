f13e0f012a9391a3e2a0f35d768d951921ca1f3c
YIFAN ZHU <154294424+Evan2001ok@users.noreply.github.com>
2024-06-10 08:13:30 +0800

    Add files via upload

---
+++ findX.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+def count_x(words):
+    times = 0
+    for time in words:
+        if time == "x" or time == "X":
+            times += 1 
+    return times
+            
+count_x("that letter isn't here")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
