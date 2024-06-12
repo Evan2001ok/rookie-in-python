2824017126302b77d964f9c77cf34e531a7f0d81
YIFAN ZHU <154294424+Evan2001ok@users.noreply.github.com>
2024-06-12 11:21:06 +0800

    Add files via upload

---
+++ FilewithDirectory function.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import os
+    os.listdir("ok")#ok file exit in pc
+    os.mkdir("ok/secondok") #make a secondok file in ok file
+    # change findx.py name and transfer to secondok file dectory
+    os.rename("ok/findX.py","ok/finY.py")
+    os.rename("ok/finY.py","ok/secondok/finY.py")
+
+
+# In[ ]:
+
+
+
+
