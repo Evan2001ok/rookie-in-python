ae55077a74b24e36b2b9df09bef6c822edf735ba
YIFAN ZHU <154294424+Evan2001ok@users.noreply.github.com>
2024-06-19 01:58:28 +0800

    Add files via upload

---
+++ animals.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+class Dog:
+    def speak(self):
+        print("woof!")
+    def eat(self,a):
+        if a == 'food':
+            print("yummy food")
+        else:
+            print("That's not food!")
+class Cat:
+    def speak(self):
+        print("Meow!")
+
+
+# In[ ]:
+
+
+
+
