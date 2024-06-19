c0eea51a1c037e0f53efffb8cf18c0d131193a96
YIFAN ZHU <154294424+Evan2001ok@users.noreply.github.com>
2024-06-19 08:48:12 +0800

    Add files via upload

--- animals.py
+++ animals.py
@@ -10,11 +10,15 @@ class Dog:
 
     def __init__(self, name):
         self.name = name
+        self.times = 0
 
     def hear(self, words):#when dog hear some one call it, dog speak
         if self.name in words:
             self.speak()
-
+    def count(self):
+        self.times += 1
+        for x in range(self.times):
+            self.speak()
 
 # In[ ]:
 
