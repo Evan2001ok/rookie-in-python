150816be876ef0ca942ef804cda0b3793c1433e4
YIFAN ZHU <154294424+Evan2001ok@users.noreply.github.com>
2024-08-07 14:26:02 +0800

    Add files via upload

---
+++ lambda_function2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def lambda_function2():  
+    cities = ["New York City", "Los Angeles", "Chicago", "Mountain View", "Denver", "Boston"]
+
+
+    short_cities = list(filter(lambda x : len(x) < 10 , cities))
+    print(short_cities)
+lambda_function2()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
